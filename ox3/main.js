3b12e3b1325027bf62cbe0a5ef772a5e594a2311
ymk <65121726+mikiymk@users.noreply.github.com>
2023-06-01 10:28:25 +0900

    ループにforを使う

--- ox3/main.js
+++ ox3/main.js
@@ -1,10 +1,13 @@
 "use strict";
 
 const main = () => {
-  const nums = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-  const boardData = [-1, -1, -1, -1, -1, -1, -1, -1, -1];
-  const ids = nums.map((n) => `ox3-${n}`);
-  const cells = ids.map((id) => document.getElementById(id));
+  const boardData = Array(9).fill(-1);
+  const cells = [];
+  for (let i = 0; i < 9; i++) {
+    const id = `ox3-${i}`;
+    const cell = document.getElementById(id);
+    cells.push(cell);
+  }
   const stat = document.getElementById("ox3-stat");
   const reset = document.getElementById("ox3-reset");
 
@@ -14,15 +17,16 @@ const main = () => {
   let mark = 1;
 
   const setBoard = () => {
-    cells.forEach((cell, index) => {
+    for (let i = 0; i < 9; i++) {
+      const cell = cells[i];
       while (cell.firstChild) cell.removeChild(cell.lastChild);
-      const cellData = boardData[index];
+      const cellData = boardData[i];
       if (cellData == 1) {
         cell.appendChild(charO.content.cloneNode(true));
       } else if (cellData == 2) {
         cell.appendChild(charX.content.cloneNode(true));
       }
-    });
+    }
   };
 
   const getWinner = () => {
@@ -38,9 +42,7 @@ const main = () => {
     ];
 
     for (const line of winnerLines) {
-      const cell1 = boardData[line[0]];
-      const cell2 = boardData[line[1]];
-      const cell3 = boardData[line[2]];
+      const [cell1, cell2, cell3] = line.map(num => boardData[num]);
 
       if (cell1 != -1 && cell1 == cell2 && cell1 == cell3) {
         return cell1;
@@ -64,14 +66,13 @@ const main = () => {
     setBoard();
   };
 
-  cells.forEach((cell, index) =>
-    cell.addEventListener("click", () => onClick(index))
-  );
+  for (let i = 0; i < 9; i++) {
+    const cell = cells[i];
+    cell.addEventListener("click", () => onClick(i));
+  }
 
   reset.addEventListener("click", () => {
-    for (let i = 0; i < 9; i++) {
-      boardData[i] = -1;
-    }
+    boardData.fill(-1);
     mark = 1;
     stat.innerText = "";
     setBoard();
