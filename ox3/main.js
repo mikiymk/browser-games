baba4cda9116a5e4ca1b32204bb4cd6820c8d346
mikiymk <mikiymk001@gmail.com>
2023-06-01 15:45:33 +0900

    関数の場所を移動する

--- ox3/main.js
+++ ox3/main.js
@@ -1,82 +1,86 @@
-"use strict";
-
-const main = () => {
+export const main = () => {
   const boardData = Array(9).fill(-1);
   const cells = [];
   for (let i = 0; i < 9; i++) {
     const id = `ox3-${i}`;
-    const cell = document.getElementById(id);
+    const cell = elem(id);
     cells.push(cell);
   }
-  const stat = document.getElementById("ox3-stat");
-  const reset = document.getElementById("ox3-reset");
 
-  const charO = document.getElementById("ox3-temp-o");
-  const charX = document.getElementById("ox3-temp-x");
+  const stat = elem("ox3-stat");
+  const reset = elem("ox3-reset");
 
   let mark = 1;
 
-  const setBoard = () => {
-    for (let i = 0; i < 9; i++) {
-      const cell = cells[i];
-      while (cell.firstChild) cell.removeChild(cell.lastChild);
-      const cellData = boardData[i];
-      if (cellData == 1) {
-        cell.appendChild(charO.content.cloneNode(true));
-      } else if (cellData == 2) {
-        cell.appendChild(charX.content.cloneNode(true));
+  for (let index = 0; index < 9; index++) {
+    const cell = cells[index];
+
+    const onClick = () => {
+      const cellData = boardData[index];
+      if (mark != -1 && cellData == -1) {
+        boardData[index] = mark;
+        mark = mark == 1 ? 2 : 1;
+        const winner = getWinner(boardData);
+        if (winner != -1) {
+          stat.innerText = winner == 1 ? "O win" : "X win";
+          mark = -1;
+        }
       }
-    }
-  };
+      setBoard(cells, boardData);
+    };
 
-  const getWinner = () => {
-    const winnerLines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-
-    for (const line of winnerLines) {
-      const [cell1, cell2, cell3] = line.map((num) => boardData[num]);
-
-      if (cell1 != -1 && cell1 == cell2 && cell1 == cell3) {
-        return cell1;
-      }
-    }
-
-    return -1;
-  };
-
-  const onClick = (number) => {
-    const cellData = boardData[number];
-    if (mark != -1 && cellData == -1) {
-      boardData[number] = mark;
-      mark = mark == 1 ? 2 : 1;
-      const winner = getWinner();
-      if (winner != -1) {
-        stat.innerText = winner == 1 ? "O win" : "X win";
-        mark = -1;
-      }
-    }
-    setBoard();
-  };
-
-  for (let i = 0; i < 9; i++) {
-    const cell = cells[i];
-    cell.addEventListener("click", () => onClick(i));
+    cell.addEventListener("click", onClick);
   }
 
-  reset.addEventListener("click", () => {
+  const onReset = () => {
     boardData.fill(-1);
     mark = 1;
     stat.innerText = "";
-    setBoard();
-  });
+    setBoard(cells, boardData);
+  };
+
+  reset.addEventListener("click", onReset);
 };
 
-document.addEventListener("DOMContentLoaded", main);
+const elem = (id) => {
+  return document.getElementById(id);
+};
+
+const getWinner = (boardData) => {
+  const winnerLines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (const line of winnerLines) {
+    const [cell1, cell2, cell3] = line.map((num) => boardData[num]);
+
+    if (cell1 != -1 && cell1 == cell2 && cell1 == cell3) {
+      return cell1;
+    }
+  }
+
+  return -1;
+};
+
+const setBoard = (cells, boardData) => {
+  const charO = elem("ox3-temp-o");
+  const charX = elem("ox3-temp-x");
+
+  for (let i = 0; i < 9; i++) {
+    const cell = cells[i];
+    while (cell.firstChild) cell.removeChild(cell.lastChild);
+    const cellData = boardData[i];
+    if (cellData == 1) {
+      cell.appendChild(charO.content.cloneNode(true));
+    } else if (cellData == 2) {
+      cell.appendChild(charX.content.cloneNode(true));
+    }
+  }
+};
