e289636251950c20365de798c1fbc07211a6dfef
mikiymk <mikiymk001@gmail.com>
2023-06-01 15:58:10 +0900

    定数を使う

--- ox3/main.js
+++ ox3/main.js
@@ -1,7 +1,24 @@
+const Empty = -1;
+const OMarked = 1;
+const XMarked = 2;
+
+const BoardLength = 9;
+
+const winnerLines = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
 export const main = () => {
-  const boardData = Array(9).fill(-1);
+  const boardData = Array(BoardLength);
   const cells = [];
-  for (let i = 0; i < 9; i++) {
+  for (let i = 0; i < BoardLength; i++) {
     const id = `ox3-${i}`;
     const cell = elem(id);
     cells.push(cell);
@@ -10,20 +27,20 @@ export const main = () => {
   const stat = elem("ox3-stat");
   const reset = elem("ox3-reset");
 
-  let mark = 1;
+  let mark = Empty;
 
-  for (let index = 0; index < 9; index++) {
+  for (let index = 0; index < BoardLength; index++) {
     const cell = cells[index];
 
     const onClick = () => {
       const cellData = boardData[index];
-      if (mark != -1 && cellData == -1) {
+      if (mark != Empty && cellData == Empty) {
         boardData[index] = mark;
-        mark = mark == 1 ? 2 : 1;
+        mark = mark == OMarked ? XMarked : OMarked;
         const winner = getWinner(boardData);
-        if (winner != -1) {
-          stat.innerText = winner == 1 ? "O win" : "X win";
-          mark = -1;
+        if (winner != Empty) {
+          stat.innerText = winner == OMarked ? "O win" : "X win";
+          mark = Empty;
         }
       }
       setBoard(cells, boardData);
@@ -33,13 +50,15 @@ export const main = () => {
   }
 
   const onReset = () => {
-    boardData.fill(-1);
-    mark = 1;
+    boardData.fill(Empty);
+    mark = OMarked;
     stat.innerText = "";
     setBoard(cells, boardData);
   };
 
   reset.addEventListener("click", onReset);
+
+  onReset();
 };
 
 const elem = (id) => {
@@ -47,39 +66,28 @@ const elem = (id) => {
 };
 
 const getWinner = (boardData) => {
-  const winnerLines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-
   for (const line of winnerLines) {
     const [cell1, cell2, cell3] = line.map((num) => boardData[num]);
 
-    if (cell1 != -1 && cell1 == cell2 && cell1 == cell3) {
+    if (cell1 != Empty && cell1 == cell2 && cell1 == cell3) {
       return cell1;
     }
   }
 
-  return -1;
+  return Empty;
 };
 
 const setBoard = (cells, boardData) => {
   const charO = elem("ox3-temp-o");
   const charX = elem("ox3-temp-x");
 
-  for (let i = 0; i < 9; i++) {
+  for (let i = 0; i < BoardLength; i++) {
     const cell = cells[i];
     while (cell.firstChild) cell.removeChild(cell.lastChild);
     const cellData = boardData[i];
-    if (cellData == 1) {
+    if (cellData == OMarked) {
       cell.appendChild(charO.content.cloneNode(true));
-    } else if (cellData == 2) {
+    } else if (cellData == XMarked) {
       cell.appendChild(charX.content.cloneNode(true));
     }
   }
