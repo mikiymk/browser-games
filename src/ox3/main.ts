11422c4754b64fc981198f5b9200a86025de186d
mikiymk <mikiymk001@gmail.com>
2023-06-06 22:24:57 +0900

    エラーを減らす

--- src/ox3/main.ts
+++ src/ox3/main.ts
@@ -8,9 +8,8 @@ const BoardLength = 9;
 
 /**
  * 勝利条件のインデックス一覧
- * @type {[number, number, number][]}
  */
-const winnerLines = [
+const winnerLines: [Index, Index, Index][] = [
   [0, 1, 2],
   [3, 4, 5],
   [6, 7, 8],
@@ -20,59 +19,63 @@ const winnerLines = [
   [0, 4, 8],
   [2, 4, 6],
 ];
+const indexes: Index[] = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 
-/**
- * マス目要素のリスト
- * @type {HTMLElement[]}
- */
-const cells = [];
-/**
- * 状況テキスト要素
- * @type {HTMLElement}
- */
-let stat;
-/**
- * リセットボタン要素
- * @type {HTMLElement}
- */
-let reset;
-/**
- * ◯テンプレート要素
- * @type {HTMLTemplateElement}
- */
-let charO;
-/**
- * ✗テンプレート要素
- * @type {HTMLTemplateElement}
- */
-let charX;
+const cells: HTMLElement[] = [];
+let stat: HTMLElement;
+let reset: HTMLElement;
+let charO: HTMLTemplateElement;
+let charX: HTMLTemplateElement;
+
+type Mark = typeof OMarked | typeof XMarked;
+type Empty = typeof Empty;
+type Index = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
+
+type BoardData = [
+  Mark | Empty,
+  Mark | Empty,
+  Mark | Empty,
+  Mark | Empty,
+  Mark | Empty,
+  Mark | Empty,
+  Mark | Empty,
+  Mark | Empty,
+  Mark | Empty,
+];
+
+type Awaitable<T> = T | Promise<T>;
+interface Player {
+  getMarkIndex(boardData: BoardData, mark: Mark): Awaitable<Index | typeof Reset>;
+}
+
+type Players = Record<Mark, Player>;
 
 /**
  * メイン関数
  */
 export const main = () => {
-  const players = {
+  const players: Players = {
     [OMarked]: humanPlayer,
     [XMarked]: aiPlayer,
   };
 
   // 要素を代入する
-  for (let i = 0; i < BoardLength; i++) {
-    const id = `ox3-${i}`;
-    const cell = elem(id);
+  for (let index = 0; index < BoardLength; index++) {
+    const id = `ox3-${index}`;
+    const cell = element(id);
     cells.push(cell);
   }
 
-  stat = elem("ox3-stat");
-  reset = elem("ox3-reset");
-  charO = elem("ox3-temp-o");
-  charX = elem("ox3-temp-x");
+  stat = element("ox3-stat");
+  reset = element("ox3-reset");
+  charO = element("ox3-temp-o") as HTMLTemplateElement;
+  charX = element("ox3-temp-x") as HTMLTemplateElement;
 
-  const settings = elem("ox3-settings");
-  const openSettings = elem("ox3-open-settings");
-  const closeSettings = elem("ox3-close-settings");
-  const playerO = elem("ox3-player-o");
-  const playerX = elem("ox3-player-x");
+  const settings = element("ox3-settings") as HTMLDialogElement;
+  const openSettings = element("ox3-open-settings");
+  const closeSettings = element("ox3-close-settings");
+  const playerO = element("ox3-player-o") as HTMLInputElement;
+  const playerX = element("ox3-player-x") as HTMLInputElement;
 
   openSettings.addEventListener("click", () => {
     settings.showModal();
@@ -83,7 +86,7 @@ export const main = () => {
   });
 
   playerO.addEventListener("change", (event) => {
-    const value = event.currentTarget.value;
+    const value = (event.currentTarget as HTMLInputElement).value;
     console.log(value);
     if (value === "human") {
       players[OMarked] = humanPlayer;
@@ -93,7 +96,7 @@ export const main = () => {
   });
 
   playerX.addEventListener("change", (event) => {
-    const value = event.currentTarget.value;
+    const value = (event.currentTarget as HTMLInputElement).value;
     if (value === "human") {
       players[XMarked] = humanPlayer;
     } else if (value === "ai") {
@@ -104,7 +107,7 @@ export const main = () => {
   playerO.value = "human";
   playerX.value = "ai";
 
-  for (let index = 0; index < BoardLength; index++) {
+  for (const index of indexes) {
     const cell = cells[index];
 
     const onClick = () => {
@@ -122,12 +125,12 @@ export const main = () => {
   const onReset = () => {
     humanPlayer.resolve(Reset);
 
-    gameLoop(players);
+    void gameLoop(players);
   };
 
   reset.addEventListener("click", onReset);
 
-  gameLoop(players);
+  void gameLoop(players);
 };
 
 const humanPlayer = {
@@ -142,21 +145,23 @@ const humanPlayer = {
 
   resolve: () => {},
 
-  getMarkIndex(boardData, mark) {
+  getMarkIndex(boardData: BoardData, mark: Mark) {
     humanPlayer.boardData = boardData;
     humanPlayer.reset();
 
-    return humanPlayer.promise;
+    return humanPlayer.promise as Promise<Index | typeof Reset>;
   },
 };
 
 const aiPlayer = {
-  getMarkIndex(boardData, mark) {
+  getMarkIndex(boardData: BoardData, mark: Mark) {
     // 各ラインで相手が2つと空きマスの場合、空きマスを選ぶ
-    const clearCells = [];
-    const interCells = [];
+    const clearCells: Index[] = [];
+    const interCells: Index[] = [];
+    const emptyCells: Index[] = [];
+
     for (const line of winnerLines) {
-      const [cell1, cell2, cell3] = line.map((num) => boardData[num]);
+      const [cell1, cell2, cell3] = line.map((number) => boardData[number]);
 
       if (cell1 === invertMark(mark) && cell1 === cell2 && cell3 === Empty) {
         interCells.push(line[2]);
@@ -180,11 +185,10 @@ const aiPlayer = {
     }
 
     // 空きマス
-    const emptyCells = [];
-    for (let i = 0; i < BoardLength; i++) {
-      const cell = boardData[i];
+    for (const index of indexes) {
+      const cell = boardData[index];
       if (cell === Empty) {
-        emptyCells.push(i);
+        emptyCells.push(index);
       }
     }
 
@@ -198,21 +202,21 @@ const aiPlayer = {
   },
 };
 
-const randomSelect = (list) => {
+const randomSelect = <T>(list: T[]): T | undefined => {
   if (list.length === 0) return undefined;
   return list[Math.floor(Math.random() * list.length)];
 };
 
-const gameLoop = async (players) => {
-  let mark = OMarked;
-  const boardData = Array(BoardLength).fill(Empty);
+const gameLoop = async (players: Players) => {
+  let mark: Mark = OMarked;
+  const boardData = Array.from<Mark | Empty>({ length: BoardLength }).fill(Empty) as BoardData;
 
   setBoard(cells, boardData);
 
-  while (true) {
+  for (;;) {
     const player = players[mark];
 
-    stat.innerText = mark === OMarked ? "O mark" : "X mark";
+    stat.textContent = mark === OMarked ? "O mark" : "X mark";
 
     const index = await player.getMarkIndex(boardData, mark);
 
@@ -228,20 +232,20 @@ const gameLoop = async (players) => {
     // 勝ちが決まったか確認する
     const winner = getWinner(boardData);
     if (winner !== Empty) {
-      stat.innerText = winner === OMarked ? "O win" : "X win";
+      stat.textContent = winner === OMarked ? "O win" : "X win";
 
       break;
     }
 
     // すべて埋まっているか確認する
     let count = 0;
-    for (let i = 0; i < BoardLength; i++) {
-      if (boardData[i] !== -1) {
+    for (const index of indexes) {
+      if (boardData[index] !== -1) {
         count++;
       }
     }
     if (count === 9) {
-      stat.innerText = "filled";
+      stat.textContent = "filled";
       break;
     }
   }
@@ -249,25 +253,23 @@ const gameLoop = async (players) => {
 
 /**
  * IDから要素を取得する関数
- * @param {string} id
- * @returns {HTMLElement}
  */
-const elem = (id) => {
-  return document.getElementById(id);
+const element = (id: string): HTMLElement => {
+  // eslint-disable-next-line unicorn/prefer-query-selector, @typescript-eslint/no-non-null-assertion
+  return document.getElementById(id)!;
 };
 
-const invertMark = (mark) => {
+const invertMark = (mark: Mark) => {
   return mark === OMarked ? XMarked : OMarked;
 };
 
 /**
  * 勝利者を取得する関数
  * いない場合は空を返す
- * @param {(-1 | 1 | 2)[]} boardData
  */
-const getWinner = (boardData) => {
+const getWinner = (boardData: (-1 | 1 | 2)[]) => {
   for (const line of winnerLines) {
-    const [cell1, cell2, cell3] = line.map((num) => boardData[num]);
+    const [cell1, cell2, cell3] = line.map((number) => boardData[number]);
 
     // データが空でなく、１・２・３が同じなら勝利
     if (cell1 !== Empty && cell1 === cell2 && cell1 === cell3) {
@@ -281,22 +283,20 @@ const getWinner = (boardData) => {
 
 /**
  * 表示ボードにデータを反映する関数
- * @param {HTMLElement[]} cells
- * @param {(-1 | 1 | 2)[]} boardData
  */
-const setBoard = (cells, boardData) => {
-  for (let i = 0; i < BoardLength; i++) {
-    const cell = cells[i];
+const setBoard = (cells: HTMLElement[], boardData: BoardData) => {
+  for (const index of indexes) {
+    const cell = cells[index];
 
     // もとの子要素をすべて削除する
-    while (cell.firstChild) cell.removeChild(cell.lastChild);
+    while (cell?.firstChild) cell.lastChild?.remove();
 
-    const cellData = boardData[i];
+    const cellData = boardData[index];
 
     if (cellData === OMarked) {
-      cell.appendChild(charO.content.cloneNode(true));
+      cell?.append(charO.content.cloneNode(true));
     } else if (cellData === XMarked) {
-      cell.appendChild(charX.content.cloneNode(true));
+      cell?.append(charX.content.cloneNode(true));
     }
   }
 };
