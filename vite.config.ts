788b4f8f579be59a2c0ac5570d880193bed3703a
mikiymk <mikiymk001@gmail.com>
2023-06-06 21:20:20 +0900

    ファイルを移動する

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,23 @@
+import { resolve, dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 // eslint-disable-next-line import/no-unresolved
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+  base: "https://mikiymk.github.io/js-html-game/",
+
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/index.html"),
+        ox3: resolve(__dirname, "src/ox3/index.html"),
+        chess: resolve(__dirname, "src/chess/index.html"),
+      },
+    },
+  },
+
   test: {},
 });
