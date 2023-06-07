a05c57c05f4d90dea609d11b2e0bae78f4117beb
mikiymk <mikiymk001@gmail.com>
2023-06-07 16:44:59 +0900

    出力フォルダをdistに修正する

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,9 @@ import { resolve } from "node:path";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+  root: "src",
   build: {
+    outDir: "../dist",
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
