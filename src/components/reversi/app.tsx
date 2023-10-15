44f11479a17993dedd510abc5aa8fba87597f817
mikiymk <mikiymk001@gmail.com>
2023-10-15 23:56:12 +0900

    add reversi stones image

--- src/components/reversi/app.tsx
+++ src/components/reversi/app.tsx
@@ -1,15 +1,19 @@
-import { For, createSignal, onMount } from "solid-js";
+import { For, Match, Switch, createSignal, onMount } from "solid-js";
 
+import blackStone from "@/images/reversi/stone-black.svg";
+import whiteStone from "@/images/reversi/stone-white.svg";
 import { cellStyle } from "@/styles/knight-tour.css";
 import { boardStyle } from "@/styles/reversi.css";
 
 const CellEmpty = 0;
+const CellBlack = 1;
+const CellWhite = 2;
 
 export const App = () => {
   const [board, setBoard] = createSignal<number[]>([]);
 
   const handleStart = () => {
-    setBoard(Array.from({ length: 64 }, () => CellEmpty));
+    setBoard(Array.from({ length: 64 }, (_, index) => index % 3));
   };
 
   onMount(() => {
@@ -21,7 +25,18 @@ export const App = () => {
       <div class={boardStyle}>
         <For each={board()}>
           {(square) => {
-            return <span class={cellStyle}>{square}</span>;
+            return (
+              <span class={cellStyle}>
+                <Switch>
+                  <Match when={square === CellBlack}>
+                    <img src={blackStone.src} alt="black stone" />
+                  </Match>
+                  <Match when={square === CellWhite}>
+                    <img src={whiteStone.src} alt="white stone" />
+                  </Match>
+                </Switch>
+              </span>
+            );
           }}
         </For>
       </div>
