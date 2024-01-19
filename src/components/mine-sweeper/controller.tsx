d55872918ed110e7c811e7d32b8fd082fb81a348
mikiymk <mikiymk001@gmail.com>
2024-01-19 22:53:15 +0900

    マインスイーパの設定を別ページに移動する

--- src/components/mine-sweeper/controller.tsx
+++ src/components/mine-sweeper/controller.tsx
@@ -1,30 +1,12 @@
-import { SettingModal } from "@/components/common/setting-modal";
 import type { JSXElement } from "solid-js";
 
 type ControllerProperties = {
-  readonly height: number;
-  readonly width: number;
-  readonly mineAmount: number;
-
   readonly message: string;
 
   readonly reset: () => void;
-  readonly setHeight: (height: number) => void;
-  readonly setWidth: (width: number) => void;
-  readonly setMineAmount: (amount: number) => void;
 };
 
 export const Controller = (properties: ControllerProperties): JSXElement => {
-  const handleHeight = (event: { readonly currentTarget: HTMLInputElement }): void => {
-    properties.setHeight(Number(event.currentTarget.value));
-  };
-  const handleWidth = (event: { readonly currentTarget: HTMLInputElement }): void => {
-    properties.setWidth(Number(event.currentTarget.value));
-  };
-  const handleMines = (event: { readonly currentTarget: HTMLInputElement }): void => {
-    properties.setMineAmount(Number(event.currentTarget.value));
-  };
-
   return (
     <div>
       status:
@@ -37,35 +19,6 @@ export const Controller = (properties: ControllerProperties): JSXElement => {
       >
         reset
       </button>
-      <SettingModal>
-        <p>
-          <label>
-            height:
-            <input type="number" min={4} value={properties.height} onChange={handleHeight} />
-          </label>
-          <br />4 ～
-        </p>
-        <p>
-          <label>
-            width:
-            <input type="number" min={4} value={properties.width} onChange={handleWidth} />
-          </label>
-          <br />4 ～
-        </p>
-        <p>
-          <label>
-            number of mines:
-            <input
-              type="number"
-              min={1}
-              max={properties.height * properties.width - 9}
-              value={properties.mineAmount}
-              onChange={handleMines}
-            />
-          </label>
-          <br />1 ～ {properties.height * properties.width - 9}
-        </p>
-      </SettingModal>
     </div>
   );
 };
