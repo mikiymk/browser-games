a7706073dd7c979ff110a999812b3ec8fda08c58
mikiymk <mikiymk001@gmail.com>
2024-04-18 18:53:03 +0900

    将棋の駒を文字から作成する

---
+++ src/games/shogi/components/piece.tsx
@@ -0,0 +1,44 @@
+import type { JSXElement } from "solid-js";
+import piece from "@/images/shogi/piece.svg";
+import { kanjiStyle, pieceStyle, redKanjiStyle,  } from "../style.css";
+
+type DefinePieceProperties = {
+  readonly id: string;
+  readonly pieces: readonly [char1: string, char2: string];
+  readonly promoted?: boolean;
+};
+
+export const DefinePiece = (properties: DefinePieceProperties): JSXElement => {
+  const kanji = (): string => {
+    return properties.promoted === true ? redKanjiStyle : kanjiStyle;
+  };
+
+  return (
+    <symbol id={properties.id} viewBox="0 0 60 60">
+      <use href={`${piece.src}#root`} height={60} width={60} class={pieceStyle} />
+      <text x="30" y="28" class={kanji()}>
+        {properties.pieces[0]}
+      </text>
+      <text x="30" y="50" class={kanji()}>
+        {properties.pieces[1]}
+      </text>
+    </symbol>
+  );
+};
+
+type UsePieceProperties = {
+  readonly x: number;
+  readonly y: number;
+
+  readonly piece: string;
+  readonly rotate: boolean;
+};
+export const UsePiece = (properties: UsePieceProperties): JSXElement => {
+  const rotate = (): string => {
+    return properties.rotate ? "" : `rotate(180, ${properties.x + 5}, ${properties.y + 5})`;
+  };
+
+  return (
+    <use href={`#${properties.piece}`} height={10} width={10} x={properties.x} y={properties.y} transform={rotate()} />
+  );
+};
--- src/games/shogi/components/square.tsx
+++ src/games/shogi/components/square.tsx
@@ -1,23 +1,8 @@
-import ginShoKanji from "@/images/shogi/gin-sho.svg";
-import gyokuShoKanji from "@/images/shogi/gyoku-sho.svg";
-import hiShaKanji from "@/images/shogi/hi-sha.svg";
-import huHyoKanji from "@/images/shogi/hu-hyo.svg";
-import kakuGyoKanji from "@/images/shogi/kaku-gyo.svg";
-import keiMaKanji from "@/images/shogi/kei-ma.svg";
-import kinShoKanji from "@/images/shogi/kin-sho.svg";
-import kyoShaKanji from "@/images/shogi/kyo-sha.svg";
-import nariGinKanji from "@/images/shogi/nari-gin.svg";
-import nariKeiKanji from "@/images/shogi/nari-kei.svg";
-import nariKyoKanji from "@/images/shogi/nari-kyo.svg";
-import oShoKanji from "@/images/shogi/o-sho.svg";
-import piece from "@/images/shogi/piece.svg";
-import ryuMeKanji from "@/images/shogi/ryu-me.svg";
-import ryuoKanji from "@/images/shogi/ryu-o.svg";
-import toKinKanji from "@/images/shogi/to-kin.svg";
 import { Show } from "solid-js";
 import type { JSXElement } from "solid-js";
 import { BISHOP, COLOR, GOLD, KING, KNIGHT, LANCE, PAWN, PIECE, PROMOTED, ROOK, SILVER } from "../constants";
-import { kanjiStyle, moveTargetStyle, pieceStyle, redKanjiStyle } from "../style.css";
+import { moveTargetStyle } from "../style.css";
+import { UsePiece } from "./piece";
 
 type SquareProperties = {
   readonly x: number;
@@ -34,45 +19,37 @@ export const Square = (properties: SquareProperties): JSXElement => {
 
     switch (properties.square & (PIECE | PROMOTED)) {
       case KING:
-        return properties.square & COLOR ? gyokuShoKanji.src : oShoKanji.src;
+        return properties.square & COLOR ? "玉将" : "王将";
       case ROOK:
-        return hiShaKanji.src;
-      case ROOK | PROMOTED:
-        return ryuoKanji.src;
+        return "飛車";
       case BISHOP:
-        return kakuGyoKanji.src;
-      case BISHOP | PROMOTED:
-        return ryuMeKanji.src;
+        return "角行";
       case GOLD:
-        return kinShoKanji.src;
+        return "金将";
       case SILVER:
-        return ginShoKanji.src;
-      case SILVER | PROMOTED:
-        return nariGinKanji.src;
+        return "銀将";
       case KNIGHT:
-        return keiMaKanji.src;
-      case KNIGHT | PROMOTED:
-        return nariKeiKanji.src;
+        return "桂馬";
       case LANCE:
-        return kyoShaKanji.src;
-      case LANCE | PROMOTED:
-        return nariKyoKanji.src;
+        return "香車";
       case PAWN:
-        return huHyoKanji.src;
+        return "歩兵";
+      case ROOK | PROMOTED:
+        return "龍王";
+      case BISHOP | PROMOTED:
+        return "龍馬";
+      case SILVER | PROMOTED:
+        return "成銀";
+      case KNIGHT | PROMOTED:
+        return "成桂";
+      case LANCE | PROMOTED:
+        return "成香";
       case PAWN | PROMOTED:
-        return toKinKanji.src;
+        return "と金";
 
       default:
         return;
     }
-  };
-
-  const style = (): string => {
-    return properties.square & PROMOTED ? redKanjiStyle : kanjiStyle;
-  };
-
-  const rotate = (): string => {
-    return properties.square & COLOR ? "" : `rotate(180, ${properties.x + 5}, ${properties.y + 5})`;
   };
 
   return (
@@ -82,26 +59,7 @@ export const Square = (properties: SquareProperties): JSXElement => {
       </Show>
       <Show when={source()}>
         {(source) => (
-          <>
-            <use
-              href={`${piece.src}#root`}
-              height={10}
-              width={10}
-              x={properties.x}
-              y={properties.y}
-              class={pieceStyle}
-              transform={rotate()}
-            />
-            <use
-              href={`${source()}#root`}
-              height={10}
-              width={10}
-              x={properties.x}
-              y={properties.y}
-              class={style()}
-              transform={rotate()}
-            />
-          </>
+          <UsePiece piece={source()} x={properties.x} y={properties.y} rotate={Boolean(properties.square & COLOR)} />
         )}
       </Show>
     </>
