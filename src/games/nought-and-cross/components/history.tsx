72944ebac835c7649e270f0df723666db5df9f61
mikiymk <mikiymk001@gmail.com>
2024-06-08 23:21:58 +0900

    マルバツゲームの過去の手の表示を変更する

--- src/games/nought-and-cross/components/history.tsx
+++ src/games/nought-and-cross/components/history.tsx
@@ -1,28 +1,54 @@
+import { HeaderButton } from "@/components/page-header/header-button";
+import { StyledSvg } from "@/components/styled-svg";
+import { PopUp } from "@/games/shogi/components/pop-up";
 import type { JSXElement } from "solid-js";
-import { For, Show } from "solid-js";
+import { For, Show, createSignal } from "solid-js";
+import cross from "@/images/icon/cross.svg";
+import nought from "@/images/icon/nought.svg";
+import history from "@/images/icon/history.svg";
 
 const historyFillArray = [-1, -1, -1, -1, -1, -1, -1, -1, -1];
 
 const position = (index: number): string => {
-  return `${"ABC"[index % 3] ?? ""}-${Math.floor(index / 3)}`;
+  return `${"ABC"[index % 3] ?? ""}-${Math.floor(index / 3) + 1}`;
 };
 
 type HistoryProperties = {
   readonly history: readonly number[];
 };
 export const History = (properties: HistoryProperties): JSXElement => {
+  const [open, setOpen] = createSignal(false);
   const paddedHistory = (): number[] => [...properties.history, ...historyFillArray].slice(0, 9);
 
   return (
-    <div class="space-x-2">
-      <span>History</span>
-      <For each={paddedHistory()}>
-        {(history) => (
-          <span class="inline-block w-12 px-2 text-center bg-lime-200">
-            <Show when={history !== -1}>{position(history)}</Show>
-          </span>
-        )}
-      </For>
-    </div>
+    <button
+      type="button"
+      onClick={() => {
+        setOpen(true);
+      }}
+    >
+      <HeaderButton icon={history.src}>History</HeaderButton>
+
+      <PopUp
+        open={open()}
+        outerClick={() => {
+          setOpen(false);
+        }}
+      >
+        <h2>History</h2>
+        <ol class="list-inside list-decimal">
+          <For each={paddedHistory()}>
+            {(history, index) => (
+              <li class=" w-32 m-auto text-left bg-yellow-100">
+                <Show when={index() % 2} fallback={<StyledSvg src={nought.src} alt="nought" />}>
+                  <StyledSvg src={cross.src} alt="cross" />
+                </Show>
+                <Show when={history !== -1}>{position(history)}</Show>
+              </li>
+            )}
+          </For>
+        </ol>
+      </PopUp>
+    </button>
   );
 };
--- src/games/shogi/components/pop-up.tsx
+++ src/games/shogi/components/pop-up.tsx
@@ -3,13 +3,25 @@ import { Show } from "solid-js";
 
 type PopUpProperties = {
   readonly open: boolean;
+  readonly outerClick?: () => void;
   readonly children: JSXElement;
 };
 export const PopUp = (properties: PopUpProperties): JSXElement => {
   return (
     <Show when={properties.open}>
-      <dialog open class="fixed inset-0 h-screen w-screen bg-[#0001]">
-        <div class="w-2/3 m-auto mt-4 p-2 text-center bg-white">{properties.children}</div>
+      <dialog
+        open
+        class="flex items-center justify-center fixed inset-0 h-screen w-screen bg-[#0001]"
+        onClick={(event) => {
+          if (event.target === event.currentTarget) {
+            properties.outerClick?.();
+          }
+        }}
+        onKeyPress={() => {
+          // empty
+        }}
+      >
+        <div class="h-4/5 w-2/3 p-4 border-slate-800 border-2 bg-yellow-100">{properties.children}</div>
       </dialog>
     </Show>
   );
