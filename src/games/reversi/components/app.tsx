aa202f5eeb975a93cebb915be04cb679439f29b7
mikiymk <mikiymk001@gmail.com>
2024-06-09 20:55:48 +0900

    リバーシの設定を変更する

--- src/games/reversi/components/app.tsx
+++ src/games/reversi/components/app.tsx
@@ -1,6 +1,7 @@
 import { doNothingFunction } from "@/scripts/do-nothing";
 import { MultiPromise } from "@/scripts/multi-promise";
-import { PlayerTypeAi, PlayerTypeHuman, playerType } from "@/scripts/player";
+import { PlayerTypeAi, PlayerTypeHuman } from "@/scripts/player";
+import type { PlayerType } from "@/scripts/player";
 import type { JSXElement } from "solid-js";
 import { createResource, createSignal } from "solid-js";
 import { CellBlack, CellCanMoveBlack, CellCanMoveWhite, CellEmpty, CellWhite } from "../const";
@@ -11,14 +12,15 @@ import { StoneCount } from "./information";
 import { PageHeader } from "@/components/page-header/page-header";
 import { PageBody } from "@/components/page-body/page-body";
 import { StartButton } from "@/components/page-header/start-button";
+import { createUrlQuerySignal } from "@/scripts/use-url-query";
+import { Settings } from "./settings";
+import { HowToPlay } from "./how-to-play";
 
 const emptyBoard: number[] = Array.from({ length: 64 }, () => CellEmpty);
 
 export const App = (): JSXElement => {
-  const query = new URLSearchParams(location.search);
-
-  const black = playerType(query.get("black"), PlayerTypeHuman);
-  const white = playerType(query.get("white"), PlayerTypeAi);
+  const [black, setBlack] = createUrlQuerySignal<PlayerType>("black", PlayerTypeHuman);
+  const [white, setWhite] = createUrlQuerySignal<PlayerType>("white", PlayerTypeAi);
 
   const [gamePlaying, setGamePlaying] = createSignal(false);
 
@@ -46,8 +48,8 @@ export const App = (): JSXElement => {
       setBoard,
       humanInput,
       {
-        black,
-        white,
+        black: black(),
+        white: white(),
       },
       () => {
         setGamePlaying(false);
@@ -81,6 +83,8 @@ export const App = (): JSXElement => {
             <StoneCount count={countBlack()} color={CellBlack} isNext={gamePlaying() && getColor?.() === CellBlack} />
             <StoneCount count={countWhite()} color={CellWhite} isNext={gamePlaying() && getColor?.() === CellWhite} />
             <StartButton start={handleStart} />
+            <Settings white={white()} black={black()} setWhite={setWhite} setBlack={setBlack} />
+            <HowToPlay />
           </>
         }
       />
---
+++ src/games/reversi/components/settings.tsx
@@ -0,0 +1,32 @@
+import type { JSXElement, Setter } from "solid-js";
+import { SelectRadio } from "@/components/input/select-radio";
+import { playerValues } from "@/scripts/player";
+import type { PlayerType } from "@/scripts/player";
+import { HeaderPopup } from "@/components/page-header/header-popup";
+
+type SettingsProperties = {
+  readonly white: PlayerType;
+  readonly black: PlayerType;
+
+  readonly setWhite: Setter<PlayerType>;
+  readonly setBlack: Setter<PlayerType>;
+};
+export const Settings = (properties: SettingsProperties): JSXElement => {
+  return (
+    <HeaderPopup icon="settings" label="Settings">
+      <h2>Settings</h2>
+
+      <dl class="grid grid-cols-2">
+        <dt>White player</dt>
+        <dd>
+          <SelectRadio name="white" values={playerValues} value={properties.white} setValue={properties.setWhite} />
+        </dd>
+
+        <dt>Black player</dt>
+        <dd>
+          <SelectRadio name="black" values={playerValues} value={properties.black} setValue={properties.setBlack} />
+        </dd>
+      </dl>
+    </HeaderPopup>
+  );
+};
