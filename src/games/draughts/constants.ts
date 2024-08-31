973b8f933eae8a2bb2d035c62703405ac15e832e
mikiymk <mikiymk001@gmail.com>
2024-08-31 19:06:10 +0900

    ページ名前の変更

--- src/games/draughts/constants.ts
+++
@@ -1,4 +0,0 @@
-export const Empty = 0;
-
-export const BlackPlayer = 1;
-export const WhitePlayer = 2;
--- src/games/english-draughts/components/app.tsx
+++ src/games/english-draughts/components/app.tsx
@@ -5,9 +5,17 @@ import board from "@/images/chess/board.svg";
 import { PageHeader } from "@/components/page-header/page-header";
 import { PageBody } from "@/components/page-body/page-body";
 import { UsePiece } from "./define";
+import { StartButton } from "@/components/page-header/start-button";
+import { initialBoard } from "../constants";
 
 export const App = (): JSXElement => {
-  const [boardData, setBoard] = createSignal<number[]>(Array.from({ length: 64 }, (_, index) => index % 3));
+  const [boardData, setBoard] = createSignal<number[]>(Array.from({ length: 64 }, () => 0));
+
+  const handleStart = (): void => {
+    console.log("start");
+
+    setBoard(initialBoard);
+  };
 
   const handleClick = (square: number, index: number): void => {
     console.log("clicked", { square, index });
@@ -17,7 +25,13 @@ export const App = (): JSXElement => {
 
   return (
     <>
-      <PageHeader />
+      <PageHeader
+        buttons={
+          <>
+            <StartButton start={handleStart} />
+          </>
+        }
+      />
       <PageBody>
         <Board height={8} width={8} data={boardData()} background={board.src} click={handleClick}>
           {(square, _, x, y) => <UsePiece piece={square} x={x} y={y} />}
