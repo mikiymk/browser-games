5cc248b48155f81281ecbe0fb0f01dac52b43117
mikiymk <mikiymk001@gmail.com>
2025-01-13 23:41:20 +0900

    メインページを日本語にする

---
+++ src/scripts/constants.ts
@@ -0,0 +1,12 @@
+export const TEXT_BUTTON_START = "開始";
+export const TEXT_BUTTON_HISTORY = "履歴";
+export const TEXT_BUTTON_SETTINGS = "設定";
+
+export const TEXT_GAME_CHESS = "チェス";
+export const TEXT_GAME_ENGLISH_DRAUGHTS = "イギリス式チェッカー";
+export const TEXT_GAME_KLONDIKE = "クロンダイク";
+export const TEXT_GAME_KNIGHT_TOUR = "ナイト・ツアー";
+export const TEXT_GAME_MINE_SWEEPER = "マインスイーパー";
+export const TEXT_GAME_NOUGHT_AND_CROSS = "三目並べ";
+export const TEXT_GAME_REVERSI = "リバーシ";
+export const TEXT_GAME_SHOGI = "将棋";
