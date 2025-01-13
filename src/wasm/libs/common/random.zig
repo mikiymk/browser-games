4c5071090fba98d755340e2b0736c76abedaf090
mikiymk <mikiymk001@gmail.com>
2025-01-13 22:27:26 +0900

    ランダムにシードの設定を追加する

--- src/wasm/libs/common/random.zig
+++ src/wasm/libs/common/random.zig
@@ -5,20 +5,29 @@ const builtin = @import("builtin");
 // common import
 const common = @import("./main.zig");
 
-test {
-    _ = @import("random.test.zig");
-}
-
 const S = struct {
     var rand_gen = std.rand.DefaultPrng.init(0xfe_dc_ba_98_76_54_32_10);
     const rand = rand_gen.random();
 };
 
 pub const random = S.rand;
+
+/// ランダムのシードを設定します。
+export fn setSeed(seed: usize) void {
+    S.rand_gen.seed(seed);
+}
+
+/// ランダムな浮動小数点数を取得します。
 pub fn getRandom() f64 {
     return S.rand.float(f64);
 }
 
+/// 指定した範囲の整数をランダムに取得します。
 pub fn getRandomIntRange(at_least: usize, less_than: usize) usize {
     return S.rand.intRangeLessThan(usize, at_least, less_than);
+}
+
+/// スライスからランダムな要素を取得します。
+pub fn getRandomItem(slice: anytype) usize {
+    return slice[getRandomIntRange(0, slice.len)];
 }
