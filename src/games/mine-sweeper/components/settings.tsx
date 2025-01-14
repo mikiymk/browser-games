554ae32c3f61f5c214e80563b638b0914704271d
mikiymk <mikiymk001@gmail.com>
2025-01-14 20:45:32 +0900

    マインスイーパのページを日本語にする

--- src/games/mine-sweeper/components/settings.tsx
+++ src/games/mine-sweeper/components/settings.tsx
@@ -1,5 +1,6 @@
 import { InputNumber } from "@/components/input/number";
 import { HeaderPopup } from "@/components/page-header/header-popup";
+import { TEXT_HEIGHT, TEXT_MINES_COUNT, TEXT_SETTINGS, TEXT_WIDTH } from "@/scripts/constants";
 import type { JSXElement, Setter } from "solid-js";
 
 type SettingsProperties = {
@@ -13,21 +14,21 @@ type SettingsProperties = {
 };
 export const Settings = (properties: SettingsProperties): JSXElement => {
   return (
-    <HeaderPopup icon="settings" label="Settings">
-      <h2>Settings</h2>
+    <HeaderPopup icon="settings" label={TEXT_SETTINGS}>
+      <h2>{TEXT_SETTINGS}</h2>
 
       <dl class="grid grid-cols-2">
-        <dt>Height</dt>
+        <dt>{TEXT_HEIGHT}</dt>
         <dd>
           <InputNumber name="height" value={properties.height} setValue={properties.setHeight} />
         </dd>
 
-        <dt>Width</dt>
+        <dt>{TEXT_WIDTH}</dt>
         <dd>
           <InputNumber name="width" value={properties.width} setValue={properties.setWidth} />
         </dd>
 
-        <dt>Mines count</dt>
+        <dt>{TEXT_MINES_COUNT}</dt>
         <dd>
           <InputNumber name="mine-count" value={properties.mineCount} setValue={properties.setMineCount} />
         </dd>
--- src/scripts/constants.ts
+++ src/scripts/constants.ts
@@ -1,10 +1,14 @@
 export const TEXT_START = "開始";
 export const TEXT_HISTORY = "履歴";
 export const TEXT_SETTINGS = "設定";
+
 export const TEXT_PLAYER = "プレイヤー";
 export const TEXT_NON_PLAYER = "コンピューター";
 export const TEXT_WHITE_PLAYER = "白プレイヤー";
 export const TEXT_BLACK_PLAYER = "黒プレイヤー";
+export const TEXT_HEIGHT = "高さ";
+export const TEXT_WIDTH = "幅";
+export const TEXT_MINES_COUNT = "地雷の数";
 
 export const TEXT_GAME_CHESS = "チェス";
 export const TEXT_GAME_ENGLISH_DRAUGHTS = "イギリス式チェッカー";
