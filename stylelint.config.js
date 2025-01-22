2a59ab1ba5fd52a093d8f90505e27db91e98216c
mikiymk <mikiymk001@gmail.com>
2025-01-23 02:10:13 +0900

    stylelintの範囲を変更

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,7 +1,6 @@
 /** @type {import('stylelint').Config} */
 export default {
   extends: "stylelint-config-standard",
-  ignoreFiles: ["node_modules/**/*.css", "dist/**/*.css"],
 
   rules: {
     // Biomeと被るルールを無効化
