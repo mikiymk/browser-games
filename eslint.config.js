8b60274e4c9ff11cfdc596dab48a031a2196ac84
mikiymk <mikiymk001@gmail.com>
2025-03-20 21:39:34 +0900

    Lintルールを追加する

--- eslint.config.js
+++ eslint.config.js
@@ -267,6 +267,40 @@ export default typescript.config(
       "unicorn/no-thenable": "off", // lint/suspicious/noThenProperty
 
       // 1.7.0 promoted
+      "barrel-files/avoid-barrel-files": "off", // performance/noBarrelFile
+      "barrel-files/avoid-re-export-all": "off", // performance/noReExportAll
+      "barrel-files/avoid-namespace-import": "off", // style/noNamespaceImport
+      // style/useNodeAssertStrict
+      "jest/no-disabled-tests": "off", // suspicious/noSkippedTests
+
+      // 1.8.0 promoted
+      "import/no-nodejs-modules": "off", // correctness/noNodejsModules
+      "no-array-constructor": "off", // correctness/useArrayLiterals
+      // correctness/noConstantMathMinMaxClamp
+      // correctness/noFlatMapIdentity
+
+      // 1.9.0 promoted
+      "no-useless-concat": "off", // complexity/noUselessStringConcat
+      "no-undef-init": "off", // complexity/noUselessUndefinedInitialization
+      "unicorn/prefer-date-now": "off", // complexity/useDateNow
+      // correctness/noUndeclaredDependencies
+      // correctness/noUnusedFunctionParameters
+      // correctness/useImportExtensions
+      // performance/useTopLevelRegex
+      "jest/no-done-callback": "off", // style/noDoneCallback
+      yoda: "off", // style/noYodaExpression
+      "no-new-wrappers": "off", // style/useConsistentBuiltinInstantiation
+      "default-case": "off", // style/useDefaultSwitchClause
+      "unicorn/explicit-length-check": "off", // style/useExplicitLengthCheck
+      "unicorn/throw-new-error": "off", // style/useThrowNewError
+      "no-throw-literal": "off", // style/useThrowOnlyError
+      "@typescript-eslint/only-throw-error": "off", // style/useThrowOnlyError
+      "no-console": "off", // suspicious/noConsole
+      // suspicious/noEvolvingTypes
+      "jest/no-standalone-expect": "off", // suspicious/noMisplacedAssertion
+      "solidjs/no-react-specific-props": "off", // suspicious/noReactSpecificProps
+      "unicorn/error-message": "off", // suspicious/useErrorMessage
+      "unicorn/require-number-to-fixed-digits-argument": "off", // suspicious/useNumberToFixedDigitsArgument
     },
   },
 
