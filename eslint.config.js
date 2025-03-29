4d21a756bae23c724f0221c8aab67b202170c335
mikiymk <mikiymk001@gmail.com>
2025-03-29 23:46:59 +0900

    eslintの設定を変更する

--- eslint.config.js
+++ eslint.config.js
@@ -10,58 +10,52 @@ import typescript from "typescript-eslint";
 
 export default typescript.config(
   {
-    files: ["**/*.ts", "**/*.tsx"],
     ignores: ["src/env.d.ts"],
-
-    plugins: {
-      "@typescript-eslint": typescript.plugin,
-      import: pluginImport,
-      unicorn,
-      solid,
-      vitest,
-      "@eslint-community/eslint-comments": eslintComments,
-    },
-
+  },
+  eslint.configs.recommended,
+  typescript.configs.strictTypeChecked,
+  typescript.configs.stylisticTypeChecked,
+  unicorn.configs.recommended,
+  solid.configs["flat/typescript"],
+  {
+    files: ["**/*.test.{ts,tsx}"],
+    plugins: { vitest },
+    rules: { ...vitest.configs.recommended.rules },
+  },
+  biome,
+  {
     languageOptions: {
       globals: {
         ...globals.builtin,
         ...globals.browser,
       },
 
-      parser: typescript.parser,
       parserOptions: {
-        sourceType: "module",
-        project: true,
-        tsconfigRootDir: "./",
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
       },
     },
 
     linterOptions: {
       noInlineConfig: false,
       reportUnusedDisableDirectives: "error",
+      reportUnusedInlineConfigs: "error",
     },
-
+  },
+  {
+    // eslint
+    rules: {},
+  },
+  {
+    // unicorn
     rules: {
-      // extends
-      ...eslint.configs.recommended.rules,
-      ...typescript.plugin.configs["eslint-recommended"].overrides[0].rules,
-      ...typescript.plugin.configs["recommended-type-checked"].rules,
-      ...typescript.plugin.configs["strict-type-checked"].rules,
-      ...typescript.plugin.configs["stylistic-type-checked"].rules,
-      ...unicorn.configs.recommended.rules,
-      ...solid.configs.typescript.rules,
-      ...vitest.configs.recommended.rules,
-      ...biome.rules,
-
-      // my custom
-
-      // eslint
-
-      // unicorn
       "unicorn/number-literal-case": "off",
       "unicorn/prefer-query-selector": "off",
-
-      // typescript
+    },
+  },
+  {
+    // typescript
+    rules: {
       "class-methods-use-this": "off",
       "@typescript-eslint/class-methods-use-this": "error",
       "default-param-last": "off",
@@ -131,148 +125,147 @@ export default typescript.config(
           allowRegExp: false,
         },
       ],
-
-      // import
-      "import/no-empty-named-blocks": "error",
-      "import/no-extraneous-dependencies": "off",
-      // biome-ignore lint/suspicious/noDuplicateObjectKeys: importがeslint-v9に対応するまでOff
-      "import/no-mutable-exports": "error",
-      "import/unambiguous": "error",
-      "import/no-absolute-path": "error",
-      "import/no-useless-path-segments": "error",
+    },
+  },
+  {
+    // import
+    plugins: { import: pluginImport },
+    rules: {
       "import/consistent-type-specifier-style": ["error", "prefer-top-level"],
-      // biome-ignore lint/suspicious/noDuplicateObjectKeys: importがeslint-v9に対応するまでOff
       "import/newline-after-import": "error",
+      "import/no-absolute-path": "error",
       "import/no-anonymous-default-export": "error",
       "import/no-duplicates": "error",
+      "import/no-empty-named-blocks": "error",
+      "import/no-extraneous-dependencies": "off",
+      "import/no-mutable-exports": "error",
       "import/no-named-default": "error",
       "import/no-unassigned-import": "error",
-
-      "import/order": [
-        "error",
-        {
-          groups: [],
-          "newlines-between": "never",
-        },
-      ],
-
-      // eslint-comments
+      "import/no-unresolved": ["error", { ignore: ["\\.css$"] }],
+      "import/no-unused-modules": ["error", { missingExports: true }],
+      "import/no-useless-path-segments": "error",
+      "import/order": ["error", { groups: [], "newlines-between": "never" }],
+      "import/unambiguous": "error",
+    },
+  },
+  {
+    // eslint-comments
+    plugins: { "@eslint-community/eslint-comments": eslintComments },
+    rules: {
       "@eslint-community/eslint-comments/no-use": ["error", { allow: ["eslint-disable-next-line"] }],
       "@eslint-community/eslint-comments/require-description": "error",
-
-      // eslint-v9に対応するまでOff
-      "import/no-mutable-exports": "off",
-      "import/newline-after-import": "off",
-
-      // unnecessary with typescript
-
+    },
+  },
+  {
+    rules: {
+      // typescriptで検出できるルール
       "import/export": "off",
       "import/default": "off",
       "import/named": "off",
       "import/namespace": "off",
-      // "import/no-unresolved": "error",
-      "import/no-unresolved": ["error", { ignore: ["\\.css$"] }],
       "import/no-named-as-default": "off",
       "import/no-named-as-default-member": "off",
+    },
+  },
+  {
+    rules: {
+      // Biomeで検出できるルール
+      "@typescript-eslint/ban-types": "off", // complexity/noBannedTypes
+      "no-extra-boolean-cast": "off", // complexity/noExtraBooleanCast
+      "unicorn/no-array-for-each": "off", // complexity/noForEach
+      "no-regex-spaces": "off", // complexity/noMultipleSpacesInRegularExpressionLiterals
+      "@typescript-eslint/no-extraneous-class": "off", // complexity/noStaticOnlyClass
+      "unicorn/no-static-only-class": "off", // complexity/noStaticOnlyClass
+      "no-useless-catch": "off", // complexity/noUselessCatch
+      "@typescript-eslint/no-useless-constructor": "off", // complexity/noUselessConstructor
+      "@typescript-eslint/no-this-alias": "off", // complexity/noUselessThisAlias
+      "unicorn/no-this-assignment": "off", // complexity/noUselessThisAlias
+      "@typescript-eslint/no-unnecessary-type-constraint": "off", // complexity/noUselessTypeConstraint
+      "no-with": "off", // complexity/noWith
+      "unicorn/prefer-array-flat-map": "off", // complexity/useFlatMap
+      "@typescript-eslint/dot-notation": "off", // complexity/useLiteralKeys
+      "@typescript-eslint/prefer-optional-chain": "off", // complexity/useOptionalChain
 
-      // unnecessary with Biome
+      "no-constant-condition": "off", // correctness/noConstantCondition
+      "no-empty-character-class": "off", // correctness/noEmptyCharacterClassInRegex
+      "no-empty-pattern": "off", // correctness/noEmptyPattern
+      "no-inner-declarations": "off", // correctness/noInnerDeclarations
+      "no-self-assign": "off", // correctness/noSelfAssign
+      "no-unsafe-finally": "off", // correctness/noUnsafeFinally
+      "no-unsafe-optional-chaining": "off", // correctness/noUnsafeOptionalChaining
+      "no-unused-labels": "off", // correctness/noUnusedLabels
+      "@typescript-eslint/no-unused-vars": "off", // correctness/noUnusedVariables
+      "use-isnan": "off", // correctness/useIsNan
+      "for-direction": "off", // correctness/useValidForDirection
+      "require-yield": "off", // correctness/useYield
 
-      "@typescript-eslint/ban-types": "off", // lint/complexity/noBannedTypes
-      "no-extra-boolean-cast": "off", // lint/complexity/noExtraBooleanCast
-      "unicorn/no-array-for-each": "off", // lint/complexity/noForEach
-      "no-regex-spaces": "off", // lint/complexity/noMultipleSpacesInRegularExpressionLiterals
-      "@typescript-eslint/no-extraneous-class": "off", // lint/complexity/noStaticOnlyClass
-      "unicorn/no-static-only-class": "off", // lint/complexity/noStaticOnlyClass
-      "no-useless-catch": "off", // lint/complexity/noUselessCatch
-      "@typescript-eslint/no-useless-constructor": "off", // lint/complexity/noUselessConstructor
-      "@typescript-eslint/no-this-alias": "off", // lint/complexity/noUselessThisAlias
-      "unicorn/no-this-assignment": "off", // lint/complexity/noUselessThisAlias
-      "@typescript-eslint/no-unnecessary-type-constraint": "off", // lint/complexity/noUselessTypeConstraint
-      "no-with": "off", // lint/complexity/noWith
-      "unicorn/prefer-array-flat-map": "off", // lint/complexity/useFlatMap
-      "@typescript-eslint/dot-notation": "off", // lint/complexity/useLiteralKeys
-      "@typescript-eslint/prefer-optional-chain": "off", // lint/complexity/useOptionalChain
+      "prefer-rest-params": "off", // style/noArguments
+      "@typescript-eslint/no-namespace": "off", // style/noNamespace
+      "unicorn/no-negated-condition": "off", // style/noNegationElse
+      "@typescript-eslint/no-non-null-assertion": "off", // style/noNonNullAssertion
+      "no-var": "off", // style/noVar
+      "@typescript-eslint/prefer-as-const": "off", // style/useAsConstAssertion
+      "prefer-const": "off", // style/useConst
+      "@typescript-eslint/prefer-literal-enum-member": "off", // style/useLiteralEnumMembers
+      "solid/self-closing-comp": "off", // style/useSelfClosingElements
+      "@typescript-eslint/array-type": "off", // style/useShorthandArrayType
+      "unicorn/switch-case-braces": "off", // style/useSingleCaseStatement
+      "no-case-declarations": "off", // style/useSingleCaseStatement
 
-      "no-constant-condition": "off", // lint/correctness/noConstantCondition
-      "no-empty-character-class": "off", // lint/correctness/noEmptyCharacterClassInRegex
-      "no-empty-pattern": "off", // lint/correctness/noEmptyPattern
-      "no-inner-declarations": "off", // lint/correctness/noInnerDeclarations
-      "no-self-assign": "off", // lint/correctness/noSelfAssign
-      "no-unsafe-finally": "off", // lint/correctness/noUnsafeFinally
-      "no-unsafe-optional-chaining": "off", // lint/correctness/noUnsafeOptionalChaining
-      "no-unused-labels": "off", // lint/correctness/noUnusedLabels
-      "@typescript-eslint/no-unused-vars": "off", // lint/correctness/noUnusedVariables
-      "use-isnan": "off", // lint/correctness/useIsNan
-      "for-direction": "off", // lint/correctness/useValidForDirection
-      "require-yield": "off", // lint/correctness/useYield
+      "no-async-promise-executor": "off", // suspicious/noAsyncPromiseExecutor
+      "no-ex-assign": "off", // suspicious/noCatchAssign
+      "no-class-assign": "off", // suspicious/noClassAssign
+      "no-compare-neg-zero": "off", // suspicious/noCompareNegZero
+      "@typescript-eslint/no-invalid-void-type": "off", // suspicious/noConfusingVoidType
+      "no-control-regex": "off", // suspicious/noControlCharactersInRegex
+      "no-debugger": "off", // suspicious/noDebugger
+      "no-duplicate-case": "off", // suspicious/noDuplicateCase
+      "solid/jsx-no-duplicate-props": "off", // suspicious/noDuplicateJsxProps
+      "@typescript-eslint/no-explicit-any": "off", // suspicious/noExplicitAny
+      "@typescript-eslint/no-extra-non-null-assertion": "off", // suspicious/noExtraNonNullAssertion
+      "no-fallthrough": "off", // suspicious/noFallthroughSwitchClause
+      "@typescript-eslint/no-misused-new": "off", // suspicious/noMisleadingInstantiator
+      "no-prototype-builtins": "off", // suspicious/noPrototypeBuiltins
+      "no-shadow-restricted-names": "off", // suspicious/noShadowRestrictedNames
+      "no-sparse-arrays": "off", // suspicious/noSparseArray
+      "@typescript-eslint/no-unsafe-declaration-merging": "off", // suspicious/noUnsafeDeclarationMerging
+      "unicorn/no-instanceof-array": "off", // suspicious/useIsArray
+      "@typescript-eslint/prefer-namespace-keyword": "off", // suspicious/useNamespaceKeyword
+      "valid-typeof": "off", // suspicious/useValidTypeof
 
-      "prefer-rest-params": "off", // lint/style/noArguments
-      "@typescript-eslint/no-namespace": "off", // lint/style/noNamespace
-      "unicorn/no-negated-condition": "off", // lint/style/noNegationElse
-      "@typescript-eslint/no-non-null-assertion": "off", // lint/style/noNonNullAssertion
-      "no-var": "off", // lint/style/noVar
-      "@typescript-eslint/prefer-as-const": "off", // lint/style/useAsConstAssertion
-      "prefer-const": "off", // lint/style/useConst
-      "@typescript-eslint/prefer-literal-enum-member": "off", // lint/style/useLiteralEnumMembers
-      "solid/self-closing-comp": "off", // lint/style/useSelfClosingElements
-      "@typescript-eslint/array-type": "off", // lint/style/useShorthandArrayType
-      "unicorn/switch-case-braces": "off", // lint/style/useSingleCaseStatement
-      "no-case-declarations": "off", // lint/style/useSingleCaseStatement
+      // 1.6.0
+      "no-unused-private-class-members": "off", // correctness/noUnusedPrivateClassMembers
+      "@typescript-eslint/no-use-before-define": "off", // correctness/noInvalidUseBeforeDeclaration
+      "@typescript-eslint/consistent-type-exports": "off", // style/useExportType
+      "unicorn/filename-case": "off", // style/useFilenamingConvention
+      "unicorn/no-for-loop": "off", // style/useForOf
+      "@typescript-eslint/prefer-for-of": "off", // style/useForOf
+      "@typescript-eslint/consistent-type-imports": "off", // style/useImportType
+      "unicorn/prefer-node-protocol": "off", // style/useNodejsImportProtocol
+      "unicorn/prefer-number-properties": "off", // style/useNumberNamespace
+      "@typescript-eslint/prefer-function-type": "off", // style/useShorthandFunctionType
+      "no-empty": "off", // suspicious/noEmptyBlockStatements
+      "no-empty-static-block": "off", // suspicious/noEmptyBlockStatements
+      "no-empty-function": "off", // suspicious/noEmptyBlockStatements
+      "@typescript-eslint/no-empty-function": "off", // suspicious/noEmptyBlockStatements
+      "no-global-assign": "off", // suspicious/noGlobalAssign
+      "no-misleading-character-class": "off", // suspicious/noMisleadingCharacterClass
+      "unicorn/no-thenable": "off", // suspicious/noThenProperty
 
-      "no-async-promise-executor": "off", // lint/suspicious/noAsyncPromiseExecutor
-      "no-ex-assign": "off", // lint/suspicious/noCatchAssign
-      "no-class-assign": "off", // lint/suspicious/noClassAssign
-      "no-compare-neg-zero": "off", // lint/suspicious/noCompareNegZero
-      "@typescript-eslint/no-invalid-void-type": "off", // lint/suspicious/noConfusingVoidType
-      "no-control-regex": "off", // lint/suspicious/noControlCharactersInRegex
-      "no-debugger": "off", // lint/suspicious/noDebugger
-      "no-duplicate-case": "off", // lint/suspicious/noDuplicateCase
-      "solid/jsx-no-duplicate-props": "off", // lint/suspicious/noDuplicateJsxProps
-      "@typescript-eslint/no-explicit-any": "off", // lint/suspicious/noExplicitAny
-      "@typescript-eslint/no-extra-non-null-assertion": "off", // lint/suspicious/noExtraNonNullAssertion
-      "no-fallthrough": "off", // lint/suspicious/noFallthroughSwitchClause
-      "@typescript-eslint/no-misused-new": "off", // lint/suspicious/noMisleadingInstantiator
-      "no-prototype-builtins": "off", // lint/suspicious/noPrototypeBuiltins
-      "no-shadow-restricted-names": "off", // lint/suspicious/noShadowRestrictedNames
-      "no-sparse-arrays": "off", // lint/suspicious/noSparseArray
-      "@typescript-eslint/no-unsafe-declaration-merging": "off", // lint/suspicious/noUnsafeDeclarationMerging
-      "unicorn/no-instanceof-array": "off", // lint/suspicious/useIsArray
-      "@typescript-eslint/prefer-namespace-keyword": "off", // lint/suspicious/useNamespaceKeyword
-      "valid-typeof": "off", // lint/suspicious/useValidTypeof
-
-      // 1.6.0 promoted
-      "no-unused-private-class-members": "off", // lint/correctness/noUnusedPrivateClassMembers
-      "@typescript-eslint/no-use-before-define": "off", // lint/correctness/noInvalidUseBeforeDeclaration
-      "@typescript-eslint/consistent-type-exports": "off", // lint/style/useExportType
-      "unicorn/filename-case": "off", // lint/style/useFilenamingConvention
-      "unicorn/no-for-loop": "off", // lint/style/useForOf
-      "@typescript-eslint/prefer-for-of": "off", // lint/style/useForOf
-      "@typescript-eslint/consistent-type-imports": "off", // lint/style/useImportType
-      "unicorn/prefer-node-protocol": "off", // lint/style/useNodejsImportProtocol
-      "unicorn/prefer-number-properties": "off", // lint/style/useNumberNamespace
-      "@typescript-eslint/prefer-function-type": "off", // lint/style/useShorthandFunctionType
-      "no-empty": "off", // lint/suspicious/noEmptyBlockStatements
-      "no-empty-static-block": "off", // lint/suspicious/noEmptyBlockStatements
-      "no-empty-function": "off", // lint/suspicious/noEmptyBlockStatements
-      "@typescript-eslint/no-empty-function": "off", // lint/suspicious/noEmptyBlockStatements
-      "no-global-assign": "off", // lint/suspicious/noGlobalAssign
-      "no-misleading-character-class": "off", // lint/suspicious/noMisleadingCharacterClass
-      "unicorn/no-thenable": "off", // lint/suspicious/noThenProperty
-
-      // 1.7.0 promoted
+      // 1.7.0
       "barrel-files/avoid-barrel-files": "off", // performance/noBarrelFile
       "barrel-files/avoid-re-export-all": "off", // performance/noReExportAll
       "barrel-files/avoid-namespace-import": "off", // style/noNamespaceImport
       // style/useNodeAssertStrict
       "jest/no-disabled-tests": "off", // suspicious/noSkippedTests
 
-      // 1.8.0 promoted
+      // 1.8.0
       "import/no-nodejs-modules": "off", // correctness/noNodejsModules
       "no-array-constructor": "off", // correctness/useArrayLiterals
       // correctness/noConstantMathMinMaxClamp
       // correctness/noFlatMapIdentity
 
-      // 1.9.0 promoted
+      // 1.9.0
       "no-useless-concat": "off", // complexity/noUselessStringConcat
       "no-undef-init": "off", // complexity/noUselessUndefinedInitialization
       "unicorn/prefer-date-now": "off", // complexity/useDateNow
@@ -295,36 +288,5 @@ export default typescript.config(
       "unicorn/error-message": "off", // suspicious/useErrorMessage
       "unicorn/require-number-to-fixed-digits-argument": "off", // suspicious/useNumberToFixedDigitsArgument
     },
-  },
-
-  // not test
-  {
-    files: ["**/*.ts", "**/*.tsx"],
-    ignores: ["src/env.d.ts", "**/*.test.ts", "**/*.test.tsx"],
-
-    plugins: {
-      import: pluginImport,
-    },
-
-    rules: {
-      "import/no-unused-modules": [
-        "warn",
-        {
-          missingExports: true,
-        },
-      ],
-    },
-  },
-
-  // test
-  {
-    files: ["**/*.test.ts", "**/*.test.tsx"],
-    ignores: [],
-
-    plugins: {
-      import: pluginImport,
-    },
-
-    rules: {},
   },
 );
