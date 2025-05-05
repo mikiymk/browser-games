3d5cd0083a6d021f059e1643fffd8856b7baf203
mikiymk <mikiymk001@gmail.com>
2025-05-06 00:35:39 +0900

    SVGのパスの文字列を作る関数を作成する

---
+++ src/common/scripts/svg-path.ts
@@ -0,0 +1,195 @@
+type Move = {
+  readonly type: "move";
+  readonly x: number;
+  readonly y: number;
+};
+/**
+ * 絶対値のMoveToコマンド。
+ * @param x X座標
+ * @param y Y座標
+ * @returns MoveToコマンドのパスオブジェクト
+ */
+export const move = (x: number, y: number): Move => {
+  return { type: "move", x, y };
+};
+const moveToText = (command: Move, previous: Previous): string => {
+  const { x, y } = command;
+
+  return shorter(`M${x},${y}`, `m${x - previous.x},${y - previous.y}`);
+};
+
+type Line = {
+  readonly type: "line";
+  readonly x: number;
+  readonly y: number;
+};
+/**
+ * 絶対値のLineToコマンド。
+ * @param x X座標
+ * @param y Y座標
+ * @returns LineToコマンドのパスオブジェクト
+ */
+export const line = (x: number, y: number): Line => {
+  return { type: "line", x, y };
+};
+const lineToText = (command: Line, previous: Previous): string => {
+  const { x, y } = command;
+
+  if (previous.x === x && previous.y === y) {
+    return ""; // noop
+  }
+
+  if (previous.x === x) {
+    return shorter(`V${y}`, `v${y - previous.y}`);
+  }
+
+  if (previous.y === y) {
+    return shorter(`H${x}`, `h${x - previous.x}`);
+  }
+
+  return shorter(`L${x},${y}`, `l${x - previous.x},${y - previous.y}`);
+};
+
+type Bezier = {
+  readonly type: "bezier";
+  readonly x: number;
+  readonly x1: number;
+  readonly x2: number;
+  readonly y: number;
+  readonly y1: number;
+  readonly y2: number;
+};
+/**
+ * 絶対値の3次ベジェ曲線コマンド。
+ * @param x1 開始制御点のX座標
+ * @param y1 開始制御点のY座標
+ * @param x2 終端制御点のX座標
+ * @param y2 終端制御点のY座標
+ * @param x 終了点のX座標
+ * @param y 終了点のY座標
+ * @returns 3次ベジェ曲線のパスオブジェクト
+ */
+export const bezier = (x1: number, y1: number, x2: number, y2: number, x: number, y: number): Bezier => {
+  return {
+    type: "bezier",
+    x,
+    x1,
+    x2,
+    y,
+    y1,
+    y2,
+  };
+};
+const bezierToText = (command: Bezier, previous: Previous): string => {
+  const absolute = `C${command.x1},${command.y1} ${command.x2},${command.y2} ${command.x},${command.y}`;
+  const relative = `c${command.x1 - previous.x},${command.y1 - previous.y} ${command.x2 - previous.x},${command.y2 - previous.y} ${command.x - previous.x},${command.y - previous.y}`;
+
+  return shorter(absolute, relative);
+};
+
+type Arc = {
+  readonly angle: number;
+  readonly largeArc: 0 | 1;
+  readonly rx: number;
+  readonly ry: number;
+  readonly sweep: 0 | 1;
+  readonly type: "arc";
+  readonly x: number;
+  readonly y: number;
+};
+/**
+ * 絶対値の楕円円弧曲線コマンド。
+ * @param rx 楕円のX半径
+ * @param ry 楕円のY半径
+ * @param angle 楕円の傾き
+ * @param largeArc 大きい円弧フラグ
+ * @param sweep 時計回りの円弧フラグ
+ * @param x 終了点のX座標
+ * @param y 終了点のY座標
+ * @returns 楕円円弧曲線のパスオブジェクト
+ */
+export const arc = (
+  rx: number,
+  ry: number,
+  angle: number,
+  largeArc: 0 | 1,
+  sweep: 0 | 1,
+  x: number,
+  y: number,
+): Arc => {
+  return {
+    angle,
+    largeArc,
+    rx,
+    ry,
+    sweep,
+    type: "arc",
+    x,
+    y,
+  };
+};
+const arcToText = (command: Arc, previous: Previous): string => {
+  const absolute = `A${command.rx},${command.ry} ${command.angle} ${command.largeArc} ${command.sweep} ${command.x},${command.y}`;
+  const relative = `a${command.rx - previous.x},${command.ry - previous.y} ${command.angle} ${command.largeArc} ${command.sweep} ${command.x - previous.x},${command.y - previous.y}`;
+
+  return shorter(absolute, relative);
+};
+
+type Close = {
+  readonly type: "close";
+};
+/**
+ * パスを閉じるコマンド。
+ * @returns パスを閉じるパスオブジェクト
+ */
+export const close = (): Close => {
+  return { type: "close" };
+};
+
+export type Path = Arc | Bezier | Close | Line | Move;
+type Previous = { readonly x: number; readonly y: number };
+const shorter = (a: string, b: string): string => (a.length < b.length ? a : b);
+
+/**
+ * SVGのパスを生成する。
+ * @param commands パスオブジェクトの配列
+ * @returns パス文字列
+ */
+export const path = (...commands: readonly Path[]): string => {
+  const previous = { x: 0, y: 0 };
+  return commands
+    .map((command) => {
+      let commandText = "";
+
+      switch (command.type) {
+        case "arc": {
+          commandText = arcToText(command, previous);
+          break;
+        }
+        case "bezier": {
+          commandText = bezierToText(command, previous);
+          break;
+        }
+        case "close":
+          return "Z";
+
+        case "line": {
+          commandText = lineToText(command, previous);
+          break;
+        }
+
+        case "move": {
+          commandText = moveToText(command, previous);
+          break;
+        }
+
+        default:
+          command satisfies never;
+      }
+
+      previous.x = command.x;
+      previous.y = command.y;
+      return commandText;
+    })
+    .join("");
+};
--- src/styles/fonts.css.ts
+++ src/styles/fonts.css.ts
@@ -1,37 +1,33 @@
 import { fontFace } from "@vanilla-extract/css";
 
-// Noto Sans Latin
-const latin = fontFace({
+/** Noto Sans Latin */
+export const latin = fontFace({
   fontDisplay: "swap",
   fontStyle: "normal",
   fontWeight: "400",
   src: "url(https://cdn.jsdelivr.net/fontsource/fonts/noto-sans@latest/latin-400-normal.woff2) format('woff2')",
 });
 
-// Noto Sans Japanese
-const japanese = fontFace({
+/** Noto Sans Japanese */
+export const japanese = fontFace({
   fontDisplay: "swap",
   fontStyle: "normal",
   fontWeight: "400",
   src: "url(https://cdn.jsdelivr.net/fontsource/fonts/noto-sans-jp@latest/japanese-400-normal.woff2) format('woff2')",
 });
 
-// Noto Color Emoji
-const emoji = fontFace({
+/** Noto Color Emoji */
+export const emoji = fontFace({
   fontDisplay: "swap",
   fontStyle: "normal",
   fontWeight: "400",
   src: "url(https://cdn.jsdelivr.net/fontsource/fonts/noto-color-emoji@latest/emoji-400-normal.woff2) format('woff2')",
 });
 
-// Material Symbols Rounded
-const symbols = fontFace({
+/** Material Symbols Rounded */
+export const symbols = fontFace({
   fontDisplay: "swap",
   fontStyle: "normal",
   fontWeight: "400",
   src: "url(https://cdn.jsdelivr.net/fontsource/fonts/material-symbols-rounded@latest/latin-400-normal.woff2) format('woff2')",
-  unicodeRange:
-    "U+0000-00FF,U+0131,U+0152-0153,U+02BB-02BC,U+02C6,U+02DA,U+02DC,U+0304,U+0308,U+0329,U+2000-206F,U+2074,U+20AC,U+2122,U+2191,U+2193,U+2212,U+2215,U+FEFF,U+FFFD",
 });
-
-export { emoji, japanese, latin, symbols };
